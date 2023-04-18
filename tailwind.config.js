ca59b078329f74888b5ff922b786fa8e8a5dfdb5
Manuel Meindl <manuel.meindl@akarion.com>
2023-04-18 07:34:48 +0200

    update content

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,7 +57,7 @@ module.exports = {
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    './app.vue',
   ],
   safelist: [
     'fade-enter-active',
